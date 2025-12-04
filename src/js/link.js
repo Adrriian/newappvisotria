93db3efad26f94b391a6cb63958bea051c66d86f
RHAIREN Pc <135435739+Adrriian@users.noreply.github.com>
2025-12-04 11:41:18 -0300

    Add files via upload

---
+++ src/js/link.js
@@ -0,0 +1,61 @@
+// link.js - cria a vistoria no Firestore + gera link do cliente
+
+// Firebase Inicialização
+const firebaseConfig = {
+  apiKey: "AIzaSyDGgk0mDtYN4V7lsGqni3buCJ2cN03jccU",
+  authDomain: "vistoria-18512.firebaseapp.com",
+  projectId: "vistoria-18512",
+  storageBucket: "vistoria-18512.firebasestorage.app",
+  messagingSenderId: "686144622304",
+  appId: "1:686144622304:web:cfa35ba985568d44d10a85",
+  measurementId: "G-M5DYEBN29V"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+// Elements
+let name_link = document.querySelector('.name_link');
+let telphone_link = document.querySelector('.telphone');
+let plate = document.querySelector('.plate');
+let button_link = document.querySelector('.button_dw2');
+
+button_link.addEventListener('click', gerar);
+
+async function gerar() {
+  let nomeCliente = name_link.value;
+  let telefoneCliente = telphone_link.value;
+  let placaCliente = plate.value;
+
+  const user = auth.currentUser;
+  if (!user) {
+    alert("Erro: usuário não logado!");
+    return;
+  }
+
+  // pegar dados do consultor
+  const consultorRef = await db.collection("consultores").doc(user.uid).get();
+  const dadosConsultor = consultorRef.data();
+
+  // criar vistoria
+  const docRef = await db.collection("vistoria").add({
+    nome: nomeCliente,
+    telefone: telefoneCliente,
+    placa: placaCliente,
+    uid_consultor: user.uid,
+    whats_consultor: dadosConsultor.telefone || "",
+    criadoEm: firebase.firestore.FieldValue.serverTimestamp()
+  });
+
+  const idGerado = docRef.id;
+
+  // gerar link para o cliente
+   const linkFinal = `https://appvistoriaweb.netlify.app/pages/vistoria.html?vistoria=${idGerado}`;
+
+  // enviar mensagem via WhatsApp
+  let texto = encodeURIComponent(`Olá! Preciso que você realize a vistoria do veículo. Acesse o link abaixo:\n${linkFinal}`);
+  window.location.href = `https://wa.me/55${telefoneCliente}?text=${texto}`;
+
+  console.log("Vistoria criada:", idGerado);
+}
